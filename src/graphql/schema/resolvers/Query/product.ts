8ac2f036772faa5abc3bc71ee98687920fefee69
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-08 14:40:45 +0100

    configure test resolver with faker.js

--- src/graphql/schema/resolvers/Query/product.ts
+++ src/graphql/schema/resolvers/Query/product.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import type { QueryResolvers } from "./../../../types.generated";
 export const product: NonNullable<QueryResolvers["product"]> = async (
 	_parent,
@@ -5,5 +6,18 @@ export const product: NonNullable<QueryResolvers["product"]> = async (
 	_ctx,
 ) => {
 	/* Implement Query.product resolver logic here */
-	return null;
+	const name = faker.commerce.productName();
+	return {
+		id: _arg.id,
+		name: name,
+		slug: name.toLowerCase().replace(/ /g, "-"),
+		price: Number(
+			faker.commerce.price({
+				min: 1,
+				max: 100,
+				dec: 2,
+			}),
+		),
+		description: faker.commerce.productDescription(),
+	};
 };
