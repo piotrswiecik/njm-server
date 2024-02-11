a29cb09f1ed75bd5ca5cd77bad7cfabd3b1f1380
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-11 19:01:14 +0100

    refactor resolver to use db

--- src/graphql/schema/resolvers/Query/product.ts
+++ src/graphql/schema/resolvers/Query/product.ts
@@ -1,23 +1,14 @@
-import { faker } from "@faker-js/faker";
+import { prisma } from "../../../../db"
 import type { QueryResolvers } from "./../../../types.generated";
 export const product: NonNullable<QueryResolvers["product"]> = async (
 	_parent,
 	_arg,
 	_ctx,
 ) => {
-	/* Implement Query.product resolver logic here */
-	const name = faker.commerce.productName();
-	return {
-		id: _arg.id,
-		name: name,
-		slug: name.toLowerCase().replace(/ /g, "-"),
-		price: Number(
-			faker.commerce.price({
-				min: 1,
-				max: 100,
-				dec: 2,
-			}),
-		),
-		description: faker.commerce.productDescription(),
-	};
+	const product = await prisma.product.findUnique({
+		where: {
+			id: _arg.id
+		}
+	});
+	return product;
 };
