32ddde0c2127e4f499f64d01ca29bab75c12bebc
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-11 18:57:01 +0100

    refactor product schema & redo migrations

--- prisma/migrations/20240211175634_init/migration.sql
+++ prisma/migrations/20240211175634_init/migration.sql
@@ -1,10 +1,13 @@
 -- CreateTable
 CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "slug" TEXT NOT NULL,
+    "artist" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
+    "coverImg" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "format" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
