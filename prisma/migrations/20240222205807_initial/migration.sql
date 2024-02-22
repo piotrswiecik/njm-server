1f6e316dbfcba52e8de942d26c037a3d01ccbe95
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-22 21:58:27 +0100

    update database schema

--- prisma/migrations/20240222205807_initial/migration.sql
+++ prisma/migrations/20240222205807_initial/migration.sql
@@ -36,6 +36,7 @@ CREATE TABLE "Track" (
 -- CreateTable
 CREATE TABLE "Variant" (
     "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "stock" INTEGER NOT NULL,
@@ -48,10 +49,8 @@ CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "artistId" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
     "coverImageUrl" TEXT NOT NULL,
     "categoryId" TEXT NOT NULL,
-    "variantId" TEXT NOT NULL,
     "releaseDate" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
@@ -64,9 +63,6 @@ CREATE TABLE "_CollectionToProduct" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Product_variantId_key" ON "Product"("variantId");
-
--- CreateIndex
 CREATE UNIQUE INDEX "_CollectionToProduct_AB_unique" ON "_CollectionToProduct"("A", "B");
 
 -- CreateIndex
@@ -76,13 +72,13 @@ CREATE INDEX "_CollectionToProduct_B_index" ON "_CollectionToProduct"("B");
 ALTER TABLE "Track" ADD CONSTRAINT "Track_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Variant" ADD CONSTRAINT "Variant_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Product" ADD CONSTRAINT "Product_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "Artist"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_variantId_fkey" FOREIGN KEY ("variantId") REFERENCES "Variant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_CollectionToProduct" ADD CONSTRAINT "_CollectionToProduct_A_fkey" FOREIGN KEY ("A") REFERENCES "Collection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
