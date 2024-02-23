b3d8e3b6571455d8c665ac9844e7e3ace3f6947e
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-23 10:28:08 +0100

    refactor search query

--- src/graphql/resolvers/Query/productSearch.ts
+++ src/graphql/resolvers/Query/productSearch.ts
@@ -7,40 +7,19 @@ export const productSearch: NonNullable<
 	/* Implement Query.productSearch resolver logic here */
 
 	try {
-		return [];
-		// logger.error(_arg.query);
-		// const productQueryResponse = await _ctx.db.product.findMany({
-		// 	include: { artist: true, coverImage: true, stock: true, category: true },
-		// 	where: {
-		// 		title: {
-		// 			contains: _arg.query,
-		// 			mode: "insensitive",
-		// 		},
-		// 	},
-		// });
-		// const productList = productQueryResponse.map((product) => {
-		// 	return {
-		// 		artist: product.artist.name,
-		// 		category: product.category,
-		// 		coverImg: {
-		// 			id: product.coverImage.id,
-		// 			width: product.coverImage.width,
-		// 			height: product.coverImage.height,
-		// 			url: product.coverImage.url,
-		// 		},
-		// 		id: product.id,
-		// 		price: product.price,
-		// 		releaseDate: product.releaseDate.toISOString(),
-		// 		stock: {
-		// 			id: product.stock.id,
-		// 			qtyCd: product.stock.qtyCd,
-		// 			qtyLp: product.stock.qtyLp,
-		// 		},
-		// 		title: product.title,
-		// 		tracks: [],
-		// 	};
-		// });
-		// return productList;
+		const dbSearchResult = await _ctx.db.product.findMany({
+			include: { artist: true },
+			where: {
+				OR: [
+					{ title: { contains: _arg.query, mode: "insensitive" } },
+					{ artist: { name: { contains: _arg.query, mode: "insensitive" } } },
+				],
+			},
+		});
+		return dbSearchResult.map((product) => ({
+			...product,
+			releaseDate: product.releaseDate.toISOString(),
+			}));
 	} catch (err) {
 		logger.error(err);
 		throw new Error(`Error searching for product`);
