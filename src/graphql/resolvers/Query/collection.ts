26cb8f01038e08e54173b5271060c9651d85cb0a
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-23 12:10:19 +0100

    add collection resolver

--- src/graphql/resolvers/Query/collection.ts
+++ src/graphql/resolvers/Query/collection.ts
@@ -5,52 +5,20 @@ export const collection: NonNullable<QueryResolvers["collection"]> = async (
 	_arg,
 	_ctx,
 ) => {
-	return null;
-	/* Implement Query.collection resolver logic here */
-	// const collection = await _ctx.db.collection.findFirst({
-	// 	where: { name: _arg.name },
-	// 	include: {
-	// 		products: {
-	// 			include: {
-	// 				product: {
-	// 					include: {
-	// 						artist: true,
-	// 						coverImage: true,
-	// 						stock: true,
-	// 						category: true,
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// });
-	// if (!collection) {
-	// 	throw new Error("Collection not found");
-	// }
-	// logger.info(collection);
-	// return {
-	// 	...collection,
-	// 	products: collection.products.map((product) => {
-	// 		return {
-	// 			artist: product.product.artist.name,
-	// 			category: product.product.category,
-	// 			coverImg: {
-	// 				id: product.product.coverImage.id,
-	// 				width: product.product.coverImage.width,
-	// 				height: product.product.coverImage.height,
-	// 				url: product.product.coverImage.url,
-	// 			},
-	// 			id: product.product.id,
-	// 			price: product.product.price,
-	// 			releaseDate: product.product.releaseDate.toISOString(),
-	// 			stock: {
-	// 				id: product.product.stock.id,
-	// 				qtyCd: product.product.stock.qtyCd,
-	// 				qtyLp: product.product.stock.qtyLp,
-	// 			},
-	// 			title: product.product.title,
-	// 			tracks: [],
-	// 		};
-	// 	}),
-	// };
+	try {
+		const dbCollection = await _ctx.db.collection.findFirst({
+			where: { name: _arg.name },
+		});
+		if (!dbCollection) {
+			throw new Error("not found");
+		}
+		return { ...dbCollection };
+	} catch (err) {
+		logger.error(err);
+		throw new Error(
+			`Error fetching collection with name=${_arg.name}: ${
+				err instanceof Error ? err.message : "unknown error"
+			}`,
+		);
+	}
 };
