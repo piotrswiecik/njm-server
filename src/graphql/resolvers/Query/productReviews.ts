a1e0098b1d49c30c6266fad10424c09414a886fa
Piotr Święcik <piotr.swiecik@gmail.com>
2024-03-01 14:50:52 +0100

    implement review query resolver

--- src/graphql/resolvers/Query/productReviews.ts
+++ src/graphql/resolvers/Query/productReviews.ts
@@ -1,7 +1,24 @@
+import { GraphQLError } from "graphql";
+import { logger } from "../../../utils/logger";
 import type { QueryResolvers } from "./../../../types.generated";
 export const productReviews: NonNullable<
 	QueryResolvers["productReviews"]
 > = async (_parent, _arg, _ctx) => {
 	/* Implement Query.productReviews resolver logic here */
-	throw new Error("Resolver not implemented");
+	try {
+		const reviews = await _ctx.db.review.findMany({
+			where: {
+				productId: _arg.productId,
+			},
+		});
+		return reviews;
+	} catch (err) {
+		if (err instanceof GraphQLError) {
+			throw err;
+		}
+		logger.error(err);
+		throw new GraphQLError(
+			`createReview mutation failed - internal error, see logs for details.`,
+		);
+	}
 };
--- src/graphql/resolvers/Review.ts
+++ src/graphql/resolvers/Review.ts
@@ -1,4 +1,56 @@
+import { GraphQLError } from "graphql";
+import { logger } from "../../utils/logger";
 import type { ReviewResolvers } from "./../../types.generated";
 export const Review: ReviewResolvers = {
 	/* Implement Review resolver logic here */
+	user: async (_parent, _arg, _ctx) => {
+		try {
+			const user = await _ctx.db.review
+				.findUnique({
+					where: {
+						id: _parent.id,
+					},
+				})
+				.user();
+			if (!user) {
+				throw new GraphQLError(
+					`resolver failed - user for review ${_parent.id} not found`,
+				);
+			}
+			return user;
+		} catch (err) {
+			if (err instanceof GraphQLError) {
+				throw err;
+			}
+			logger.error(err);
+			throw new GraphQLError(
+				`resolver failed - internal error, see logs for details.`,
+			);
+		}
+	},
+	product: async (_parent, _arg, _ctx) => {
+		try {
+			const product = await _ctx.db.review
+				.findUnique({
+					where: {
+						id: _parent.id,
+					},
+				})
+				.product();
+			if (!product) {
+				throw new GraphQLError(
+					`resolver failed - product for review ${_parent.id} not found`,
+				);
+			}
+			return { ...product, releaseDate: product.releaseDate.toISOString() };
+		} catch (err) {
+			if (err instanceof GraphQLError) {
+				throw err;
+			}
+			logger.error(err);
+			throw new GraphQLError(
+				`resolver failed - internal error, see logs for details.`,
+			);
+		}
+	},
 };
